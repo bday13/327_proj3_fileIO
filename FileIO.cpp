53c6f474e117bf333bb8f25014323d27a3d4c14e
Brandon Day <brandon.day.17@cnu.edu>
2020-04-11 16:24:52 -0700

    done

--- FileIO.cpp
+++ FileIO.cpp
@@ -6,6 +6,7 @@
  */
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "../327_proj3_test/includes/FileIO.h"
 #include "../327_proj3_test/includes/constants.h"
 
@@ -13,12 +14,39 @@ using namespace std;
 
 int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
 {
-	//TODO fill in
+	ifstream myFile;
+	myFile.open(filename);
+
+	if (!myFile.is_open()) {
+		myFile.clear();
+		return COULD_NOT_OPEN_FILE_TO_READ;
+	}
+
+	string str;
+	contents = "";
+
+	while (!myFile.eof()) {
+		getline(myFile, str, '\n');
+		contents.append(str);
+	}
+
+	return SUCCESS;
 }
 
 int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
 {
-	//TODO fill in
+	ofstream myFile;
+	myFile.open(filename);
+
+	if (!myFile.is_open()) {
+		return COULD_NOT_OPEN_FILE_TO_WRITE;
+	}
+
+	for (unsigned int i = 0; i < myEntryVector.size(); i++) {
+		myFile << myEntryVector[i] << endl;
+	}
+
+	return SUCCESS;
 }
 
 
